95ac5d29a0b8e79b72293127d7ccde328d224734
Astrutrope <134558977+Astrutrope@users.noreply.github.com>
2026-03-13 20:43:37 +0000

    inputs

--- testing.java
+++ testing.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class testing {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -8,5 +10,12 @@ public class testing {
         System.out.println("Net Worth: " + networth + " dollars");
         char grade = 'A';
         System.out.println("Grade: " + grade);
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        
+        scanner.close();
     }
 }
