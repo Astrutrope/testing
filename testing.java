d00fd4f0d417202ec7ecd300a07cdc5da9c3c01a
Astrutrope <134558977+Astrutrope@users.noreply.github.com>
2026-03-13 11:25:20 +0000

    testing java

---
+++ testing.java
@@ -0,0 +1,12 @@
+public class testing {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("2026");
+        int age = 21;
+        System.out.print("Age: " + age);
+        double networth = 1000000.50;
+        System.out.println("Net Worth: " + networth + " dollars");
+        char grade = 'A';
+        System.out.println("Grade: " + grade);
+    }
+}
